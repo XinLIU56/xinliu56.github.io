4e018382b7814e5e975dbed2c1f1a9d9a8a51c86
XinLIU56 <106716317+XinLIU56@users.noreply.github.com>
2025-04-24 12:35:55 +0000

    Deploying to gh-pages from @ XinLIU56/xinliu56.github.io@98209fe9308f30b61d21eb57ac67e9b870a036ad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-research",
           title: "Research",
           description: "",
